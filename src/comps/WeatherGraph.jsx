7d7e9d77888ea8d750aff177a9d49dcb851740ac
Oliver <oliverstaehr96@gmail.com>
2022-04-29 00:03:11 +0200

    WeatherGraph with axis

--- src/comps/WeatherGraph.jsx
+++ src/comps/WeatherGraph.jsx
@@ -1,38 +1,37 @@
 import { weatherApiResponse } from "../utils/apiTestResponse";
-
-import { Group } from "@visx/group";
-import { Line , LinePath } from "@visx/shape";
-import { scaleLinear } from "@visx/scale";
 import Svg from "../styles/WeatherGraphStyle";
-import { curveBasis , curveCatmullRom } from "@visx/curve";
-import cityTemperature from '@visx/mock-data/lib/mocks/cityTemperature';
 
-// weatherApiResponse.hourly = weatherApiResponse.hourly.map(d => {
-//   d.dt = new Date(d.dt * 1000).getHours()
-//   return d;
-// });
+// visx
+import { Group } from "@visx/group";
+import { LinePath } from "@visx/shape";
+import { scaleLinear } from "@visx/scale";
+import { curveCatmullRom } from "@visx/curve";
+import { AxisBottom } from "@visx/axis";
 
 const WeatherGraph = () => {
   
+  const margin = { top: 20, right: 20, bottom: 50, left: 20 };
   const width = 500;
   const height = 200;
-  const margin = { top: 20, right: 20, bottom: 20, left: 20 };
+  const xMax = width - margin.left - margin.right;
+  const yMax = height - margin.top - margin.bottom;
   
   const timeScale = scaleLinear({
     domain: [Math.min(...weatherApiResponse.hourly.map(d => d.dt)), Math.max(...weatherApiResponse.hourly.map(d => d.dt))],
-    range: [0, width - margin.left - margin.right],
+    range: [0, xMax],
     round: true
   });
   
   const tempScale = scaleLinear({
     domain: [Math.min(...weatherApiResponse.hourly.map(d => d.temp)), Math.max(...weatherApiResponse.hourly.map(d => d.temp))],
-    range: [height - margin.top - margin.bottom, 0],
+    range: [yMax, 0],
     round: true
   });
   
   return (
     <Svg width={width} height={height} >
       <Group top={margin.top} left={margin.left} >
+        <AxisBottom tickFormat={(d) => new Date(d * 1000).getHours()} top={yMax} scale={timeScale} numTicks={20}/>
         <LinePath
           data={weatherApiResponse.hourly}
           x={(d) => timeScale(d.dt)}
--- src/styles/WeatherGraphStyle.js
+++ src/styles/WeatherGraphStyle.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 const Svg = styled.svg`
-  stroke: black;
-  stroke-width: 5;
+  /* stroke: black; */
+  /* stroke-width: 5; */
   
 `;
 
