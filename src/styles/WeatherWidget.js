6e7c3d232d461501264c4ebcda9d20cb029adab9
Oliver <Oliverstaehr96@gmail.com>
2022-06-14 12:32:42 +0200

    change width

--- src/styles/WeatherWidget.js
+++ src/styles/WeatherWidget.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 const WeatherWidget = styled.div`
   display: grid;
-  grid-template-columns: repeat(3, 200px);
-  grid-template-rows: repeat(2, 200px);
+  grid-template-columns: repeat(3, 300px);
+  grid-template-rows: repeat(2, 300px);
   align-items: center;
   justify-items: center;
 `;
